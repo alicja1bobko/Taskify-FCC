1c76423262eefe296971462226a11f56653ac01b
AlicjaBobko <alicja_bobko@o2.pl>
2022-08-29 13:04:58 +0200

    basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-
+import React from "react";
 import "./App.css";
+import InputField from "./components/InputField";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField />
+    </div>
+  );
+};
 
 export default App;
