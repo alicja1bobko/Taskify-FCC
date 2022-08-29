1912e2b99d63ab3b43b31e0dba2cf46bb15ac5fb
AlicjaBobko <alicja_bobko@o2.pl>
2022-08-29 17:37:55 +0200

    toto list and single todo component

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 
-function InputField() {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+function InputField({ todo, setTodo, handleAdd }: Props) {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box" />
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        type="input"
+        placeholder="Enter a task"
+        className="input__box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
       <button className="input__submit" type="submit">
         Go
       </button>
